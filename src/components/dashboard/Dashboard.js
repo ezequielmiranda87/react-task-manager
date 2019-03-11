c470c375fbda1c5a9f27d6c41608ffea6b041cb7
ezex87 <ezequielmiranda87@gmail.com>
2019-03-11 12:51:46 +0100

    dev/ State connected with app

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 
 import Notifications from './Notifications';
 import ProjectList from '../project/ProjectList';
+import {connect} from 'react-redux';
 
 class Dashboard extends Component {
     render(){
+        console.log(this.props)
         return(
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList/>
+                        <ProjectList projects = {this.props.projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications/>
@@ -20,4 +22,10 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,13 @@
-const initialState = { };
+const initialState = {
+    projects: [
+        {id:1, title: ' Create a Spaceship', contenet: 'Est laboris duis ad culpa proident commodo.'},
+        {id:2, title: ' Join a crew', content: 'Commodo non aliquip nisi magna duis aliquip sit nisi nulla incididunt dolor laboris ea duis.'},
+        {id:3, title: ' Fly to Space', content: 'Exercitation qui duis deserunt ex exercitation.'},
+    ]
+ };
 
 const projectReducer = (state = initialState, action) => {
-    return action;
+    return state;
 }
 
 export default projectReducer;
