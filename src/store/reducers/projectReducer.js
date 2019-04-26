8a28b1937d8bfe3a904d8545af31e88ff45d63f8
ezex87 <ezequielmiranda87@gmail.com>
2019-04-26 20:08:51 +0200

    Projects reducers created

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,18 +1,35 @@
 const initialState = {
-    projects: [
-        {id:1, title: ' Create a Spaceship', contenet: 'Est laboris duis ad culpa proident commodo.'},
-        {id:2, title: ' Join a crew', content: 'Commodo non aliquip nisi magna duis aliquip sit nisi nulla incididunt dolor laboris ea duis.'},
-        {id:3, title: ' Fly to Space', content: 'Exercitation qui duis deserunt ex exercitation.'},
-    ]
- };
+  projects: [
+    {
+      id: 1,
+      title: " Create a Spaceship",
+      contenet: "Est laboris duis ad culpa proident commodo."
+    },
+    {
+      id: 2,
+      title: " Join a crew",
+      content:
+        "Commodo non aliquip nisi magna duis aliquip sit nisi nulla incididunt dolor laboris ea duis."
+    },
+    {
+      id: 3,
+      title: " Fly to Space",
+      content: "Exercitation qui duis deserunt ex exercitation."
+    }
+  ]
+};
 
 const projectReducer = (state = initialState, action) => {
-    switch(action.type){
-        case 'CREATE_PROJECT':
-            console.log('created project', action.project)
-    }
-
-    return state;
-}
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("created project", action.project);
+      return state;
+    case "CREATE_PROJECT_ERROR":
+      console.log("created project errr", action.err);
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default projectReducer;
