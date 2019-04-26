ae67c02cbb1cfc03e14bc829a0edb42a99a7e60b
ezex87 <ezequielmiranda87@gmail.com>
2019-04-26 19:52:45 +0200

    Minor fix

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,12 +4,7 @@ import 'firebase/auth';
 
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBp4eLn2__p9rI6jVX6eyJVOjGJoJmcUhU",
-    authDomain: "react-task-manager-8f1dd.firebaseapp.com",
-    databaseURL: "https://react-task-manager-8f1dd.firebaseio.com",
-    projectId: "react-task-manager-8f1dd",
-    storageBucket: "react-task-manager-8f1dd.appspot.com",
-    messagingSenderId: "493483207393"
+
   };
   firebase.initializeApp(config);
   
